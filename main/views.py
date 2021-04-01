76a78d45f306fe68a01bac65afe21b3aa1ee4f11
Andrey Tsakhlo <14sobaka228@gmail.com>
2021-04-01 22:04:45 +0300

    lesson3

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("<h4>Проверка работы</h4>")
+	return HttpResponse("<h4>Proverka rabotu</h4>")
 
 def about(request):
 	return HttpResponse("<h4>Stranica pro nas</h4>")
